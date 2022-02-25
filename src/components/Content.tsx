eecab62bd59584e954b7928bbfdbb75e9df7fadc
Vinicius Petrachin <vr.petrachin@gmail.com>
2022-02-24 22:22:52 -0300

    chore: split the app into more components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,31 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+interface ContentProps {
+  movies: MovieProps[];
+}
+
+export default function Content({ movies }: ContentProps) {
+  return (
+    <div className="movies-list">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.imdbID}
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+        />
+      ))}
+    </div>
+  );
 }
